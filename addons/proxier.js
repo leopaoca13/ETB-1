a35590fa638c407cd88165aa44c10ba5bfea6858
Embedded77 <59213351+Embedded77@users.noreply.github.com>
2021-05-18 09:51:46 -0400

    Update proxier.js

--- addons/proxier.js
+++ addons/proxier.js
@@ -1,4 +1,3 @@
-// do not run in same repl/program as bot
 var app=require("express")()
 app.use(require("body-parser").json())
 const fetch=require("node-fetch")
@@ -16,5 +15,16 @@ app.post("/v1/trades/send",(req,res)=>{
     })
   })
 })
-
+app.get("/v1/users/:id/can-trade-with",(req,res)=>{
+    headers={'Content-Type': 'application/json'}
+  headers.cookie=req.headers.cookie
+  headers["X-CSRF-TOKEN"]= (req.headers["X-CSRF-TOKEN"] || req.headers["x-csrf-token"])
+  fetch(`https://trades.roblox.com/v1/users/${(parseInt(req.params.id || "1")||1)}/can-trade-with`,{headers:headers}).then((tres)=>{
+    console.log("response get")
+    tres.text().then(tres=>{
+    console.log(tres)
+    res.send(tres)
+    })
+  })
+})
 app.listen(8080)
